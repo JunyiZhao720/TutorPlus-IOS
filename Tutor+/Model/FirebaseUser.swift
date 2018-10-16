cf9eca90b8bf47cd1e9dec7848b8d9df32436ff9
jzhao33 <zjylh720@gmail.com>
2018-10-16 15:05:26 -0700

    10002 Basic model for FirebaseUser

--- Tutor+/Model/FirebaseUser.swift
+++ Tutor+/Model/FirebaseUser.swift
@@ -11,4 +11,42 @@ import Firebase
 
 class FirebaseUser{
     
+    static let shared = FirebaseUser()
+    
+    var currentUser: User?
+    var userId: String? = ""
+    
+    private var listenHandler: AuthStateDidChangeListenerHandle?
+    
+    private init(){}
+    
+    func addUserListener(loggedIn: Bool){
+        print("Add listener")
+        listenHandler = Auth.auth().addStateDidChangeListener{(auth,user) in
+            if user == nil{
+                // This means we are logged out
+                print("Logged Out")
+                self.currentUser = nil
+                self.userId = ""
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    if loggedIn{
+                        
+                    } else {
+                        
+                    }
+                }
+            }
+            else{
+                print("Logged In")
+                
+                self.currentUser = user
+                self.userId = (user?.uid)
+                // LOAD DATA HERE
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+                    // Do something if logged in
+                }
+            }
+        }
+    }
 }
