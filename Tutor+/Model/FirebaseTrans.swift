744768c36254e197407ce0b8fac58ebd494c5c8e
MacOS-1.14 <zjylh720@gmail.com>
2018-10-19 18:32:22 -0700

    10004 Trying to solve Firebase import issue

---
+++ Tutor+/Model/FirebaseTrans.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseTrans.swift
+//  Tutor+
+//
+//  Created by MacOS-1.14 on 10/19/18.
+//  Copyright © 2018 JunyiZhao. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FirebaseTrans: NSObject {
+
+}
