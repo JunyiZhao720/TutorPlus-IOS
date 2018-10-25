0a7a9217d44bd37cc311636d3b318bbbbbf3212e
jzhao33 <zjylh720@gmail.com>
2018-10-25 11:43:25 -0700

    20010 Changed class and grade views

--- Tutor+/Controller/TutorProfileEdit.swift
+++ Tutor+/Controller/TutorProfileEdit.swift
@@ -2,7 +2,7 @@
 //  TutorProfileEdit.swift
 //  Tutor+
 //
-//  Created by 孙可天  on 10/20/18.
+//  Created by Bo Lan  on 10/20/18.
 //  Copyright © 2018 JunyiZhao. All rights reserved.
 //
 
@@ -11,8 +11,10 @@ import UIKit
 class TutorProfileEdit: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    var className = [String]()
     
+    @IBOutlet weak var classGradeTableView: UITableView!
+    var className = [String]()
+    var classGrade = [String]()
     
     @IBAction func addingClass(_ sender: UIButton) {
         let alert = UIAlertController(title: "Add Class", message: nil, preferredStyle: .alert)
@@ -20,20 +22,37 @@ class TutorProfileEdit: UIViewController {
         }
         let action = UIAlertAction(title: "Add", style: .default){ (_) in guard let tempclass = alert.textFields?.first?.text else{return}
             print(tempclass)
-            //self.className.append(tempclass)
-            //self.tableView.reloadData()
-            self.add(tempclass)
+            self.addClassFunc(tempclass)
         }
         alert.addAction(action)
         present(alert,animated: true)
     }
     
-    func add(_ classs: String){
+    @IBAction func btn_addingGrades(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Add Grade", message: nil, preferredStyle: .alert)
+        alert.addTextField{ (dessertTF) in dessertTF.placeholder = "Enter Grade"
+        }
+        let action = UIAlertAction(title: "Add", style: .default){ (_) in guard let tempgrade = alert.textFields?.first?.text else{return}
+            print(tempgrade)
+            self.addClassGradeFunc(tempgrade)
+        }
+        alert.addAction(action)
+        present(alert,animated: true)
+    }
+    
+    func addClassFunc(_ classs: String){
         let index = 0
         className.insert(classs, at: index)
         let indexPath = IndexPath(row: index, section: 0)
         tableView.insertRows(at: [indexPath], with: .fade)
         
+    }
+    
+    func addClassGradeFunc(_ classs: String){
+        let index = 0
+        classGrade.insert(classs, at: index)
+        let indexPath = IndexPath(row: index, section: 0)
+        classGradeTableView.insertRows(at: [indexPath], with: .fade)
     }
     
 }
@@ -49,15 +68,28 @@ extension TutorProfileEdit: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        let classes = className[indexPath.row]
-        cell.textLabel?.text = classes
+        if(tableView == classGradeTableView){
+            //let cell = UITableViewCell()
+            let grades = classGrade[indexPath.row]
+            cell.textLabel?.text = grades
+        }else{
+            // let cell = UITableViewCell()
+            let classes = className[indexPath.row]
+            cell.textLabel?.text = classes
+        }
         return cell
+        
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if(tableView == classGradeTableView){
+            guard editingStyle == .delete else {return}
+            classGrade.remove(at: indexPath.row)
+            classGradeTableView.deleteRows(at: [indexPath], with: .automatic)
+        }else{
         guard editingStyle == .delete else {return}
         className.remove(at: indexPath.row)
-        
         tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
 }
