5b03f12081d9a0a37cd54385c9fc84df66714b62
ksun13 <ksun13@ucsc.edu>
2018-10-26 21:37:05 -0700

    10008 Add table view and navigation controller for tutor result by Bo Lan

---
+++ Tutor+/Controller/TutorResultListView.swift
@@ -0,0 +1,22 @@
+//
+//  TutorResultListView.swift
+//  Tutor+
+//
+//  Created by Bo Lan  on 10/26/18.
+//  Copyright © 2018 JunyiZhao. All rights reserved.
+//
+
+import UIKit
+
+class TutorResultListView: UIViewController {
+
+    
+    @IBOutlet weak var TutorListView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
