c9388aa33bb93330153dcbe1050d6b25ae02d17e
Veronica Ng <Yueting@ZhengBingdeMacBook-Pro.local>
2018-11-01 17:39:01 -0700

    update navigator

--- Tutor+/Controller/SearchViewController.swift
+++ Tutor+/Controller/SearchViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBAction func searchBar(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
