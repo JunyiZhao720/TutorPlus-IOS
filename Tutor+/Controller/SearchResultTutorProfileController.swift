c92a7a2d0cac56502f098b3fcb59fdca217e1d3b
Vash Wang <hwang161@ucsc.edu>
2018-11-14 15:28:09 -0800

    schedule update
    schedule update with nice color
    
    Co-Authored-By: vash1996 <hwang161@ucsc.edu>

--- Tutor+/Controller/SearchResultTutorProfileController.swift
+++ Tutor+/Controller/SearchResultTutorProfileController.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class SearchResultTutorProfileController: UIViewController {
+    //schedule
+    var date: Array<Character> = Array(repeating: "0", count: 28)
+    @IBOutlet var dateBtn: [UIButton]!
+    //btn manager
+    @IBAction func dateClicked(_ sender: UIButton) {
+        if dateBtn[sender.tag].backgroundColor == UIColor.gray{
+            dateBtn[sender.tag].backgroundColor = UIColor.init(red: 0.20, green: 0.47, blue: 0.96, alpha: 1.0)
+            date[sender.tag] = "1"
+            print(("string: "), getDate())
+        }else{
+            dateBtn[sender.tag].backgroundColor = UIColor.gray
+            date[sender.tag] = "0"
+            print(("string: "), getDate())
+        }
+    }
+    //end of schedule
 
     @IBOutlet weak var tutorName: UILabel!
     @IBOutlet weak var className: UILabel!
@@ -24,7 +40,7 @@ class SearchResultTutorProfileController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateDate()
         tutorName.text = tName
         //className.text = cName
         img.image = image1
@@ -39,6 +55,31 @@ class SearchResultTutorProfileController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func getDate()-> String{
+        return toString()
+    }
     
-    
+    //这下面写个setter，
+    //schedule getter and setter function
+    func setDate(){   //<--假设你pass个string下来叫 dateDownloade
+        var dateDownloade = "0010100010111101010010010101"
+        let update = Array(dateDownloade)
+        for i in 0...27{
+            date[i] = update[i]
+        }
+    }
+    func updateDate (){
+        //这里call setter
+        setDate()
+        for i in 0...27{
+            if date[i] == "1"{dateBtn[i].backgroundColor = UIColor.init(red: 0.20, green: 0.47, blue: 0.96, alpha: 1.0)}
+            else{dateBtn[i].backgroundColor = UIColor.gray}
+        }
+    }
+    //tostring
+    func toString()-> String{
+        let stringDate = String(date)
+        return stringDate
+    }
+    //end of schedule
 }
