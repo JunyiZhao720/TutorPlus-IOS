0a639c6c90b39cd1a56f50776e1ac91fb002f1f8
jzhao33 <zjylh720@gmail.com>
2018-11-18 14:48:34 -0800

    10013 Changed User Profile Edit formats

--- Tutor+/Controller/UserProfileEditController.swift
+++ Tutor+/Controller/UserProfileEditController.swift
@@ -10,57 +10,9 @@ import UIKit
 
 class UserProfileEditController: UIViewController{
     
-// schedule
-    var date: Array<Character> = Array(repeating: "0", count: 28)
-
-    @IBOutlet var dataBtn: [UIButton]!
-
-    @IBAction func dateClicked(_ sender: UIButton) {
-        if dataBtn[sender.tag].backgroundColor == UIColor.gray{
-            dataBtn[sender.tag].backgroundColor = UIColor.init(red: 0.20, green: 0.47, blue: 0.96, alpha: 1.0)
-            date[sender.tag] = "1"
-            print(("string: "), getDate())
-        }else{
-            dataBtn[sender.tag].backgroundColor = UIColor.gray
-            date[sender.tag] = "0"
-            print(("string: "), getDate())
-        }
-    }
-
-    func getDate()-> String{
-        return toString()
-    }
-
-    //这下面写个setter，
-
-    func setDate(){   //<--假设你pass个string下来叫 dateDownloade
-        var dateDownloade = "0010100010111101010010010101"
-        let update = Array(dateDownloade)
-        for i in 0...27{
-            date[i] = update[i]
-        }
-    }
     
-    func updateDate (){
-        //这里call setter
-        setDate()
-        for i in 0...27{
-            if date[i] == "1"{dataBtn[i].backgroundColor = UIColor.init(red: 0.20, green: 0.47, blue: 0.96, alpha: 1.0)}
-            else{dataBtn[i].backgroundColor = UIColor.gray}
-        }
-    }
     
-    //tostring
-    func toString()-> String{
-        let stringDate = String(date)
-        return stringDate
-    }
-    //end of schedule
-    
-//    @IBAction func back(_ sender: UIButton) {
-//        print("clicked")
-//    }
-//
+
     @IBAction func back(_ sender: UIBarButtonItem) {
         print("clicked back buttom")
     }
@@ -90,60 +42,69 @@ class UserProfileEditController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        updateDate() //update schedule
+        // Update Schedule
+        updateDate()
         
+        // Initialize navigation bar titile
+        self.navigationItem.title = "Profile Edit"
+        
+        // Do initialization
+        initializePersonalStatementTextField()
+        initializeFirebaseInfo()
+        initializeImage()
+       
+        // Calling gender dropdown
+        createGenderPicker()
+        createToolbar()
+
+        // Reload courseTableView
+        courseTableView.reloadData()
+        courseTableView.tableFooterView = UIView(frame: CGRect.zero)
+    }
+    
+    // ------------------------------------------------------------------------------------
+    // Initialization functions
+    
+    private func initializeFirebaseInfo(){
         nameEditor.text = FirebaseUser.shared.name
         emailEditor.text = FirebaseUser.shared.email
         genderTextBox.text = FirebaseUser.shared.gender
         majorEditor.text = FirebaseUser.shared.major
-        universityEditor.text =
-            FirebaseUser.shared.university
-
-        //set image to circle
-        theImage.layer.cornerRadius = theImage.frame.size.width/2
-        theImage.clipsToBounds = true
-        theImage.layer.borderColor = UIColor.white.cgColor
-        
-        // initialize bottomOffset for tutor switch
-//        self.bottomOffset = CGPoint(x: 0, y: scrollView.contentSize.height - scrollView.bounds.size.height)
-//        debugHelpPrint(type: ClassType.UserProfileEditController, str: "bottomOffest loaded: \(bottomOffset.debugDescription)")
-        
-        //calling gender dropdown
-        createGenderPicker()
-        createToolbar()
-//        switchForTutor.isOn = false
-        courseTableView.reloadData()
-        
-        super.viewDidLoad()
-        courseTableView.tableFooterView = UIView(frame: CGRect.zero)
-        
+        universityEditor.text = FirebaseUser.shared.university
+    }
+    
+    private func initializePersonalStatementTextField(){
         personalState.text = "Personal Statement. Watch my video here: www.youtube.com"
-        
         personalState.backgroundColor = UIColor(hue: 0.5333, saturation: 0.02, brightness: 0.94, alpha: 1.0)
-        
         personalState.font = UIFont.systemFont(ofSize: 20)
         personalState.textColor = UIColor.black
-
         personalState.font = UIFont.boldSystemFont(ofSize: 20)
         personalState.font = UIFont(name:"Verdana", size: 17)
-
         personalState.isEditable = true
         personalState.autocapitalizationType = UITextAutocapitalizationType.allCharacters
-
         personalState.isSelectable = true
         personalState.isEditable = false
         personalState.dataDetectorTypes = UIDataDetectorTypes.link
-        
         personalState.isEditable = true
         personalState.autocorrectionType = UITextAutocorrectionType.yes
         personalState.spellCheckingType = UITextSpellCheckingType.yes
         personalState.autocapitalizationType = UITextAutocapitalizationType.none
-        
     }
     
-
-    // Save Button
+    private func initializeImage(){
+        theImage.layer.cornerRadius = theImage.frame.size.width/2
+        theImage.clipsToBounds = true
+        theImage.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    // End initialization functions
+    // ------------------------------------------------------------------------------------
+    
+    
+    // ------------------------------------------------------------------------------------
+    // Buttons
+    
+    // save button
     @IBAction func saveProfile(_ sender: Any) {
         //FirebaseUser.shared.image = theImage.UIImage
         FirebaseUser.shared.name = nameEditor.text
@@ -164,9 +125,7 @@ class UserProfileEditController: UIViewController{
         self.performSegue(withIdentifier: "ProfileEditToProfile", sender: self)
     }
     
-    
-    
-    // Tutor Swtich
+    // tutor swtich
     @IBAction func tutorSwitchValueChanged(_ sender: Any) {
        
         if (tutorSwitch.isOn == true) {
@@ -181,6 +140,13 @@ class UserProfileEditController: UIViewController{
             self.tutorStatus.text = "Sorry, not now"
         }
     }
+
+    // End buttons
+    // ------------------------------------------------------------------------------------
+    
+    
+    // ------------------------------------------------------------------------------------
+    // Dropdown menu
     
     //gender dropdown Picker
     func createGenderPicker(){
@@ -188,7 +154,6 @@ class UserProfileEditController: UIViewController{
         genderPicker.delegate = self
         genderTextBox.inputView = genderPicker
     }
-    
     
     //gender dropdown-- adding "Done" button
     func createToolbar(){
@@ -205,23 +170,23 @@ class UserProfileEditController: UIViewController{
         view.endEditing(true)
     }
     
+    // End Dropdown menu
+    // ------------------------------------------------------------------------------------
     
+    
+    // ------------------------------------------------------------------------------------
+    // Course Listview
     
     @IBOutlet weak var addSchool: UITextField!
     @IBOutlet weak var addClass: UITextField!
     @IBOutlet weak var addGradeText: UITextField!
     
     @IBAction func addCell(_ sender: Any) {
-        insert()
-    }
-    func insert(){
         schoolData.append(addSchool.text!)
         classData.append(addClass.text!)
         gradeData.append(addGradeText.text!)
         
         let indexPath = IndexPath(row: schoolData.count - 1, section: 0)
-//        let indexPath = IndexPath(row: classData.count - 1, section: 0)
-//        let indexPath = IndexPath(row: gradeData.count - 1, section: 0)
         
         courseTableView.beginUpdates()
         courseTableView.insertRows(at: [indexPath], with: .automatic)
@@ -230,8 +195,68 @@ class UserProfileEditController: UIViewController{
         addSchool.text = ""
         view.endEditing(true)
     }
+    
+    // End Course Listview
+    // ------------------------------------------------------------------------------------
+    
+    
+    
+
+    // ------------------------------------------------------------------------------------
+    // Schedule
+    
+    var date: Array<Character> = Array(repeating: "0", count: 28)
+    
+    @IBOutlet var dataBtn: [UIButton]!
+    
+    @IBAction func dateClicked(_ sender: UIButton) {
+        if dataBtn[sender.tag].backgroundColor == UIColor.gray{
+            dataBtn[sender.tag].backgroundColor = UIColor.init(red: 0.20, green: 0.47, blue: 0.96, alpha: 1.0)
+            date[sender.tag] = "1"
+            print(("string: "), getDate())
+        }else{
+            dataBtn[sender.tag].backgroundColor = UIColor.gray
+            date[sender.tag] = "0"
+            print(("string: "), getDate())
+        }
+    }
+    
+    func getDate()-> String{
+        return toString()
+    }
+    
+    //这下面写个setter，
+    
+    func setDate(){   //<--假设你pass个string下来叫 dateDownloade
+        let dateDownloade = "0010100010111101010010010101"
+        let update = Array(dateDownloade)
+        for i in 0...27{
+            date[i] = update[i]
+        }
+    }
+    
+    func updateDate (){
+        //这里call setter
+        setDate()
+        for i in 0...27{
+            if date[i] == "1"{dataBtn[i].backgroundColor = UIColor.init(red: 0.20, green: 0.47, blue: 0.96, alpha: 1.0)}
+            else{dataBtn[i].backgroundColor = UIColor.gray}
+        }
+    }
+    
+    //tostring
+    func toString()-> String{
+        let stringDate = String(date)
+        return stringDate
+    }
+    
+    // End Schedule
+    // ------------------------------------------------------------------------------------
 
 }
+
+// ------------------------------------------------------------------------------------
+// Delegates
 
 //gender Dropdown
 extension UserProfileEditController: UIPickerViewDelegate, UIPickerViewDataSource{
@@ -257,11 +282,6 @@ extension UserProfileEditController: UIPickerViewDelegate, UIPickerViewDataSourc
 }
 
 extension UserProfileEditController: UITableViewDataSource, UITableViewDelegate {
-    
-    
-    // For tableView
-//    tableView.delegate = self
-//    tableView.dataSource = self
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return classData.count
@@ -292,16 +312,12 @@ extension UserProfileEditController: UITableViewDataSource, UITableViewDelegate 
 
 extension UIViewController: TableViewNew {
     func onClick(index: Int) {
-    
+
         print("\(index) is clicked")
     }
 }
 
-// already done in the func, dont need to call this func
-//extension UIImageView {
-//    func setRounded() {
-//        let radius = CGGectGetWidth(self.frame) / 2
-//        self.layer.cornerRadius = radius
-//        self.layer.masksToBounds = true;
-//    }
-//}
+// End Delegates
+// ------------------------------------------------------------------------------------
+
+
