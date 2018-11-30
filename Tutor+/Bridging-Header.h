19494680bd830428099412f642fb883206df7009
jzhao33 <zjylh720@gmail.com>
2018-11-30 15:22:32 -0800

    20008 chat setting done

---
+++ Tutor+/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  Chat
+//
+//  Created by 孙可天  on 11/28/18.
+//  Copyright © 2018 Bo_Lan_try. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <JSQMessagesViewController/JSQMessages.h>
+#endif /* Bridging_Header_h */
